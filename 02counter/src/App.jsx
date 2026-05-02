aab623849313d54a3c8113196917fc61e5673f08
AnshPatel1406 <154533366+AnshPatel1406@users.noreply.github.com>
2026-05-02 16:57:59 +0000

    useState HOOK for Chnaging of Counter | add upper & lower limit to counter as a mini project

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -1,22 +1,32 @@
-import { useState } from 'react'
+import { useState } from 'react' // Hooks Comes From Here 
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 
-
+// Here We use useState Hook to change state 
 
 function App() {
-  let counter = 5
+// useState return 2 values in array form a counter and a function
+// isLoggedin , setLoggedin | login , setLogin etc.... 
+  let [counter,setCounter] = useState(15)
+
+  // let counter = 5
  
   const addValue = ()=> {
-    counter += 1
+    // counter += 1 // when we use Hook | useState we dont update like this 
+    setCounter(counter+1)
+
     console.log("Clicked",counter);
     // here when the button is clicked in console we see counter is updating 
     // but in Display h2 the value of counter does not Chnage
     // Here the Main Role of React Comes into Play
 
     // in core JS we do it by doucment.getElement and then chnaging the innerHTML but in react it makes it easy 
+  }
+
+  const subValue = () => {
+    setCounter(counter-1)
   }
   return (
     <>
@@ -26,7 +36,7 @@ function App() {
     onClick={addValue} // dont do addValue() // as we need to execute when button is clicked not before 
     >Add Value</button>
     <br/>
-    <button>Decrease Value</button>
+    <button onClick={subValue}>Decrease Value</button>
     </>
   )
 }

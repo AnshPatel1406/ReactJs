f11ee08c71b123c6df6ae2b7b24fb028f2740602
AnshPatel1406 <154533366+AnshPatel1406@users.noreply.github.com>
2026-05-02 16:03:38 +0000

    React.createElement and passing custom function written in JS and render in HTML | How the backend of it works

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,9 +1,31 @@
-import { StrictMode } from 'react'
+import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client' 
 import App from './App.jsx'
 
+function MyApp(){ // Dont Forget to Name All Function Capital
+  return (
+    <div>
+      Just a Function 
+      {/* This Function is Converted in Tree Structure which we say ing customReact thing we made our own Tree and sent to html  */}
+    </div>
+  )
+}
+const TestElement = (<a href='http://google.com' target='_blank'> Visit Google</a>)
+
+// Creating Element using React
+// we can make custom tree structure object also as we did in customReact but we need a renderfucntion to doucment.createElement and setAttrubte
+// here React has inbuilt React.createElement 
+
+const reactElement = React.createElement(
+  'a',
+  {href :'http://google.com',target:'_blank'},
+  'Click Me to Visit Google'
+)
+
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  // MyApp() // will run 
+  // <MyApp/> // this means the function is executed 
+  // TestElement // this will work // TestElement() or <TestElement/> will not Work 
+  reactElement
 )

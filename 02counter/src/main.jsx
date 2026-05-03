b670b635606bfe0e31dae79cb29d66203d8d7730
AnshPatel1406 <154533366+AnshPatel1406@users.noreply.github.com>
2026-05-03 05:02:50 +0000

    how createRoot work , updations problems and underastanding React Fibre Architecture

--- 02counter/src/main.jsx
+++ 02counter/src/main.jsx
@@ -8,3 +8,19 @@ createRoot(document.getElementById('root')).render(
     <App />
   </StrictMode>,
 )
+
+
+// here the method createRoot makes a browser like DOM 
+// here it compares 2 DOMs and only update what thing is updated in UI 
+// jo chize chnage hui hai wo purane DOM se nikal ke naya laga do 
+
+// now problem is if there is a constant update in the  UI then every senond or given time DOM updates 
+// instead we can optimize it and update the 2-3 things at once to save our resources 
+
+// but in browser the DOM everytime reloads whole root
+
+
+
+// React Fiber Architecture 
+
+// https://github.com/acdlite/react-fiber-architecture

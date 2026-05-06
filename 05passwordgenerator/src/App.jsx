d836de1174dc9cb9234d9775f5cad2ef98536224
AnshPatel1406 <154533366+AnshPatel1406@users.noreply.github.com>
2026-05-06 04:13:28 +0000

    Understanding Password Generator Code

--- 05passwordgenerator/src/App.jsx
+++ 05passwordgenerator/src/App.jsx
@@ -13,8 +13,14 @@ function App() {
   const [password, setPassword] = useState("")
 
   //useRef hook
+
   const passwordRef = useRef(null)
 
+
+  // Use Call Back is a Hook that keeps the function in cache and is called evertime the variables passed in arguments cahnge\
+  // we pass a call back function and the dependencies in array format after that 
+  // here we have passed  [length, numberAllowed, charAllowed, setPassword]) as dependencies 
+  // here setPassword is a function , we are passing reference not calling it  by ()
   const passwordGenerator = useCallback(() => {
     let pass = ""
     let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
@@ -29,15 +35,29 @@ function App() {
 
     setPassword(pass)
 
-
+// here setPassword is given instead of passowrd, if password is given it will go in infinite loop 
   }, [length, numberAllowed, charAllowed, setPassword])
 
+  // useCallback is used for optimizitaion , it can be done withouth it also 
   const copyPasswordToClipboard = useCallback(() => {
-    passwordRef.current?.select();
-    passwordRef.current?.setSelectionRange(0, 999);
+    // use of useRef hook
+    // below 2 lines are just for optimization 
+    // they give selection effect so user know it is copied 
+    // without it the work will be done but the text will not be selected when button is presssed 
+    // just for user experience 
+
+    // ? is used here because its optional, it can be null 
+    passwordRef.current?.select(); // highlights the text 
+    passwordRef.current?.setSelectionRange(0, 999); // selection range // if given 0,3 only first 3 are highlighted and copied
+
+    // here react compiles in browser so there is window object
+    // in next js the thing is rendered on server side so there is not window object there  
     window.navigator.clipboard.writeText(password)
   }, [password])
 
+  // hook to run the passGenerator method evertime changes is made on dependencies 
+  // useCallBack is for optimizing the code , the function stays in cache and is run whenever needed 
+  // we can make it withouth useCallBack function also 
   useEffect(() => {
     passwordGenerator()
   }, [length, numberAllowed, charAllowed, passwordGenerator])
@@ -54,7 +74,7 @@ function App() {
             className="outline-none w-full py-1 px-3"
             placeholder="Password"
             readOnly
-            ref={passwordRef}
+            ref={passwordRef} // this is needed to useRef Hook
         />
         <button
         onClick={copyPasswordToClipboard}
